7ac08df0a1a28aa40701ba5fe0ddfb2ddae5a56d
Josef-Kretz <86017280+Josef-Kretz@users.noreply.github.com>
2023-01-21 12:44:54 -0500

    working on front end

--- apps/client/src/Anime.js
+++ apps/client/src/Anime.js
@@ -4,9 +4,9 @@ import PassButton from './components/PassButton'
 
 const Anime = ({anime, setLoggedIn}) => {
     const [vids, setVids] = useState([])
+    const [trailer, setTrailer] = useState('')
 
-   useEffect(() => {
-    async function test() {
+    const grabAnimes = async () => {
         let res = await fetch('/api/trailer')
         let data = await res.json()
 
@@ -14,13 +14,54 @@ const Anime = ({anime, setLoggedIn}) => {
         console.log(data)
         setVids(data.data.Page.media)
     }
-    test()
+
+    const grabTrailer = async (idMal) => {
+        let res = await fetch(`https://api.jikan.moe/v4/anime/${idMal}/videos`)
+        let data = await res.json()
+        
+        setTrailer(data.data.promo[0].trailer.embed_url)
+    }
+
+    const incrementVid = async () => {
+        if(vids.length)
+        {
+            try{
+                let res = await fetch(`https://api.jikan.moe/v4/anime/${vids[0].idMal}/videos`)
+                let data = await res.json()
+        
+                setTrailer(data.data.promo[0].trailer.embed_url)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        else
+        {
+            try{
+                let res = await fetch('/api/trailer')
+                let data = await res.json()
+                    .then( () => setVids(data.data.Page.media))
+                    .then( () => incrementVid())
+
+                if(data.isError) return
+
+                
+                //setVids(data.data.Page.media)
+
+                //incrementVid()
+            }catch(err){
+                console.log(err)
+            }
+        }
+    }
+
+   useEffect(() => {
+    if(vids.length === 0) incrementVid()
    }, [])
 
     if(vids.length){
         return <>
         <h1>{vids[0].title.english ? vids[0].title.english : vids[0].title.romaji}</h1>
-        <iframe src={vids[0].trailer.id ? 'https://youtube.com/embed/'+vids[0].trailer.id : 'https://youtu.be/jfKfPfyJRdk'} />
+        <iframe src={trailer} />
         <p>{vids[0].description}</p>
         <SmashButton />
         <PassButton vids={vids} setVids={setVids} />
