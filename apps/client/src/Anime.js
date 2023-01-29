34c30c64704cdfbade1b9678e1c1f42226fde6ab
Josef-Kretz <86017280+Josef-Kretz@users.noreply.github.com>
2023-01-29 12:12:27 -0500

    improving styling on anime page

--- apps/client/src/Anime.js
+++ apps/client/src/Anime.js
@@ -1,5 +1,8 @@
 import {useEffect, useState} from 'react'
 import Carousel from 'react-bootstrap/Carousel'
+import Badge from 'react-bootstrap/Badge'
+import Accordion from 'react-bootstrap/Accordion'
+
 import SmashButton from './components/SmashButton'
 import PassButton from './components/PassButton'
 
@@ -15,17 +18,45 @@ const Anime = ({anime, setLoggedIn}) => {
     const incrementVid = () => setNextVid(!nextVid)
 
     const loadCarousel = () => {
-
-        if(trailer)
+        if(trailer.length)
         {    return <Carousel className='trailerCarousel' interval={null}>
                 {trailer.map(vid => {
-                    const key = vid.match(/\/(?<key>[\w]+)\?/).groups.key
+                    const match = vid.match(/\/(?<key>[^\?]+)/)
+                    //double ternary, check for match, check for groups: resolves annoying edge cases
+                    //at worst use full vid url as key
+                    const key = match ? match.groups ? match.groups.key : '' : ''
                     return <Carousel.Item key={key||vid}>
                         <iframe src={vid} className="trailerItem" alt="a fabulous anime trailer that will change your world" />
                     </Carousel.Item>
                 })}
             </Carousel>
             }
+    }
+
+    const loadTags = () => {
+        const tags = vids[0].tags.slice(0,5)
+        if(tags.length){
+            return <Accordion defaultActiveKey={0} flush alwaysOpen>
+                {tags.map((tag, ind) => {
+                    return <Accordion.Item key={tag.id} eventKey={ind}>
+                        <Accordion.Header>{tag.category}</Accordion.Header>
+                        <Accordion.Body>{tag.description}</Accordion.Body>
+                    </Accordion.Item>
+                })}
+            </Accordion>
+        }
+    }
+
+    const loadGenres = () => {
+        const genres = vids[0].genres
+        if(genres.length){
+            return <section>
+                {genres.map(gen => {
+                    const key = gen
+                    return <Badge pill bg='info' key={key} >{gen}</Badge>
+                })}
+            </section>
+        }
     }
 
    useEffect(()=>{
@@ -83,10 +114,11 @@ const Anime = ({anime, setLoggedIn}) => {
             <section>
             <img className='animeBanner' src={vids[0].bannerImage || ''} />
             <h1>{vids[0].title.english ? vids[0].title.english : vids[0].title.romaji}</h1>
-            {/* add tags/genres here */}
+            {loadGenres()}
             </section>
             {loadCarousel()}
-        <p className='animeDesc'>{vids[0].description}</p> 
+        <p className='animeDesc'>{vids[0].description}</p>
+        {loadTags()}
         <SmashButton incrementVid={incrementVid} animeId={vids[0].id} />
         <PassButton incrementVid={incrementVid} animeId={vids[0].id} />
     </section>
